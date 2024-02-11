b3434eb28bdcc98205f83b93a85d27542f9da3ff
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-02-11 10:47:09 -0300

    fonts

--- fonts/install_fonts.py
+++ fonts/install_fonts.py
@@ -1,7 +1,9 @@
 import os
 
 def main() -> None:
-    os.system("sudo cp ./*.ttf /usr/local/share/fonts")
+    path_fonts = "/usr/local/share/fonts"
+    os.system(f"mkdir -p {path_fonts}")
+    os.system(f"sudo cp ./*.ttf {path_fonts}")
 
 if __name__ == "__main__":
     main()
