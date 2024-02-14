5c548cb37a079fbc7226229cc8162d8be54375cc
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-02-14 02:50:10 -0300

    pastebin

--- paste_bin.sh
+++ paste_bin.sh
@@ -1,7 +1,7 @@
 sudo pacman -S git bspwm sxhkd polybar picom kitty
 sudo pacman -S ranger rofi scrot xclip feh
-sudo pacman -S pulseaudio pulseaudio-alsa pamixer
-sudo pacman -S xorg xorg-xinit xorg-xrandr arandr
+sudo pacman -S pulseaudio pulseaudio-alsa pamixer playerctl
+sudo pacman -S xorg xorg-xinit xorg-xrandr arandr xorg-xev
 sudo pacman -S firefox curl base-devel ntfs-3g dpkg code
 sudo pacman -S htop neofetch cmatrix figlet tree
 sudo pacman -S vlc
