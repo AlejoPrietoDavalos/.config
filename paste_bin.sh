f7fb869b4053ec716a68816977aebdadfa7b27a6
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-03-23 23:15:37 -0300

    jq

--- paste_bin.sh
+++ paste_bin.sh
@@ -1,5 +1,5 @@
 sudo pacman -S git bspwm sxhkd polybar picom kitty
-sudo pacman -S ranger rofi scrot xclip feh
+sudo pacman -S ranger rofi scrot xclip feh jq
 sudo pacman -S pulseaudio pulseaudio-alsa pamixer playerctl
 sudo pacman -S xorg xorg-xinit xorg-xrandr arandr xorg-xev
 sudo pacman -S firefox curl base-devel ntfs-3g dpkg code
