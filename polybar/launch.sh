0b9d41dc5d1c0c816e9791d62858d5b07571a127
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-04-03 12:40:09 -0300

    launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,10 +6,6 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-
-
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar example &
@@ -17,5 +13,3 @@ if type "xrandr"; then
 else
     polybar --reload example &
 fi
-
-echo "Bars launched..."
