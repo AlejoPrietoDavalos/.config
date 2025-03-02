07088641b2dfc8adf05c68bf537eeb4d15c19d01
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-03-02 23:09:13 +0000

    Muevo configuración a resources

--- _refactor/fonts/install_fonts.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main() -> None:
-    path_fonts = "/usr/local/share/fonts"
-    os.system(f"mkdir -p {path_fonts}")
-    os.system(f"sudo cp ./*.ttf {path_fonts}")
-
-if __name__ == "__main__":
-    main()
