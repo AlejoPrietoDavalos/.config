92007377c478f1cafe71f1eed43147b6a4180995
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2025-03-20 01:20:36 +0000

    Agrego paquetes snap, yay y pacman

--- install_packages.sh
+++ install_packages.sh
@@ -2,10 +2,16 @@
 
 PACMAN_FILE="packages_pacman.txt"
 YAY_FILE="packages_yay.txt"
+SNAP_FILE="packages_snap.txt"
+
 
 PACKAGES_PACMAN=$(cat "$PACMAN_FILE")
 PACKAGES_YAY=$(cat "$YAY_FILE")
+PACKAGES_SNAP=$(cat "$SNAP_FILE")
 
+
+
+# Intala yay si no estuviera ya instalado.
 if ! command -v yay &> /dev/null; then
     echo "--> Instalando yay..."
     sudo pacman -Syu --noconfirm
@@ -20,20 +26,51 @@ else
     echo "--> yay ya está instalado, no es necesario instalarlo."
 fi
 
+
+
+# Instala snap si no estuviera ya instalado.
+if ! command -v snap &> /dev/null; then
+    echo "--> Instalando snap..."
+    git clone https://aur.archlinux.org/snapd.git
+    cd snapd
+    makepkg -si --noconfirm
+    sudo systemctl enable --now snapd.socket
+    sudo systemctl enable --now snapd.apparmor.service
+    sudo ln -s /var/lib/snapd/snap /snap
+    cd ..
+    rm -rf snapd
+    snap version
+else
+    echo "--> snap ya está instalado, no es necesario instalarlo."
+fi
+
+
+
+# Instalar paquetes de pacman si hay paquetes listados
 if [[ -z "$PACKAGES_PACMAN" ]]; then
     echo "--> No hay paquetes en $PACMAN_FILE."
-    exit 1
+else
+    echo "--> Instalando paquetes de pacman: $PACKAGES_PACMAN"
+    sudo pacman -S --needed --noconfirm $PACKAGES_PACMAN
 fi
 
-echo "--> Instalando paquetes de pacman: $PACKAGES_PACMAN"
-sudo pacman -S --needed --noconfirm $PACKAGES_PACMAN
 
+# Instalar paquetes de yay si hay paquetes listados
 if [[ -z "$PACKAGES_YAY" ]]; then
     echo "--> No hay paquetes en $YAY_FILE."
-    exit 1
+else
+    echo "--> Instalando paquetes de yay: $PACKAGES_YAY"
+    yay -S --needed --noconfirm $PACKAGES_YAY
 fi
 
-echo "--> Instalando paquetes de yay: $PACKAGES_YAY"
-yay -S --noconfirm $PACKAGES_YAY
+
+# Instalar paquetes de snap si hay paquetes listados
+if [[ -z "$PACKAGES_SNAP" ]]; then
+    echo "--> No hay paquetes en $SNAP_FILE."
+else
+    echo "--> Instalando paquetes de snap: $PACKAGES_SNAP"
+    sudo snap install $PACKAGES_SNAP
+fi
+
 
 echo "--> Instalación completada."
